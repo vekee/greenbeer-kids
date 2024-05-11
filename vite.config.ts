6c12f695a408e371d0b6029804c24c00861594bf
Dahai Duan <vekee66@gmail.com>
2024-05-11 22:48:21 +0900

    fix prod

--- vite.config.ts
+++ vite.config.ts
@@ -10,33 +10,37 @@ import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue({
-      template: { transformAssetUrls }
-    }),
-    vueJsx(),
-    Vuetify(),
-    Components(),
-    ViteFonts({
-      google: {
-        families: [
-          {
-            name: 'Roboto',
-            styles: 'wght@100;300;400;500;700;900'
-          }
-        ]
-      }
-    })
-  ],
-  define: { 'process.env': {} },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const isProd = mode === 'production'
+  return {
+    base: isProd ? '/greenbeer-kids/' : './',
+    plugins: [
+      vue({
+        template: { transformAssetUrls }
+      }),
+      vueJsx(),
+      Vuetify(),
+      Components(),
+      ViteFonts({
+        google: {
+          families: [
+            {
+              name: 'Roboto',
+              styles: 'wght@100;300;400;500;700;900'
+            }
+          ]
+        }
+      })
+    ],
+    define: { 'process.env': {} },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
+      extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue']
     },
-    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue']
-  },
-  server: {
-    port: 3000
+    server: {
+      port: 3000
+    }
   }
 })
