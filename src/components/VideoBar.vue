56fd6ae3eab90ca6ec190031abd44ad3379cf036
Dahai Duan <vekee66@gmail.com>
2024-05-15 22:48:23 +0900

    fix

--- src/components/VideoBar.vue
+++ src/components/VideoBar.vue
@@ -10,10 +10,6 @@
       fade-img-on-scroll
       scroll-target="#scrolling-techniques-3"
     >
-      <template v-slot:img="{ props }">
-        <v-img v-bind="props" gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"></v-img>
-      </template>
-
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
       <v-app-bar-title>Hello Kids</v-app-bar-title>
