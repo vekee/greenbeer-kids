95cac29f1090b3262e382d78954d5b5757534a2e
Dahai Duan <vekee66@gmail.com>
2024-05-16 22:15:43 +0900

    add eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,9 +3,11 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
+    'plugin:@typescript-eslint/recommended',
     'plugin:vue/vue3-essential',
     'eslint:recommended',
+    '@vue/typescript/recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
