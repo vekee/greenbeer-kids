1be69689d05db86f9248def10edf0b7ff39f23ee
Dahai Duan <vekee66@gmail.com>
2024-07-05 01:10:10 +0900

    add download

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -21,6 +21,37 @@ export default {
     videoUrl: String
   },
   mounted() {
+    // カスタムボタンを定義
+    const Button = videojs.getComponent('Button')
+    class DownloadButton extends Button {
+      constructor(player, options) {
+        super(player, options)
+        this.controlText('Download Video')
+        this.addClass('vjs-icon-download')
+      }
+
+      handleClick() {
+        const link = document.createElement('a')
+        link.href = this.player_.options_.videoUrl
+        link.download = this.getFileName(this.player_.options_.videoUrl)
+        document.body.appendChild(link)
+        link.click()
+        document.body.removeChild(link)
+      }
+
+      getFileName(url) {
+        return url.split('/').pop()
+      }
+    }
+
+    // カスタムボタンを登録
+    videojs.registerComponent('DownloadButton', DownloadButton)
+
+    // カスタムボタンをコントロールバーに追加
+    this.player.ready(() => {
+      this.player.getChild('controlBar').addChild('DownloadButton', {})
+    })
+
     // Video.js の初期化
     this.player = videojs(this.$refs.videoPlayer, this.options, function onPlayerReady() {
       console.log('onPlayerReady', this)
@@ -36,5 +67,8 @@ export default {
 </script>
 
 <style scoped>
-/* カスタムスタイルを追加 */
+.video-js {
+  width: 286px; /* 幅を設定 */
+  height: 216px; /* 高さを設定 */
+}
 </style>
