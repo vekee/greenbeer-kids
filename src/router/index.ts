2f1dc7984cc4ab0523bc80cae8d10424f7dcdc33
Dahai Duan <vekee66@gmail.com>
2024-07-19 20:38:16 +0900

    fix refresh error

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,6 +9,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '',
+      name: 'index',
+      component: LoginView
+    },
+    {
       path: '/',
       name: 'index',
       component: LoginView
